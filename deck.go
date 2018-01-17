2875f490dc717cf6f3393a1db0e7ed4c6696c996
shamil9 <shamil9@gmail.com>
2018-01-17 13:10:32 +0100

    NewDeckFromFile function

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -18,12 +19,25 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) ToString() string {
+func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func (d deck) SaveToFile(filename string) error {
-	stringBytes := []byte(d.ToString())
+func (d deck) saveToFile(filename string) error {
+	stringBytes := []byte(d.toString())
 
 	return ioutil.WriteFile(filename, stringBytes, 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	stringBytes, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
+	stringSlices := strings.Split(string(stringBytes), ",")
+
+	return deck(stringSlices)
 }
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Card", newCard(), "Another card"}
-	cards = append(cards, "Fourth card")
+	cards := newDeckFromFile("cards.txt")
 
-	cards.SaveToFile("cards.txt")
-}
-
-func newCard() string {
-	return "New card"
+	cards.print()
 }
